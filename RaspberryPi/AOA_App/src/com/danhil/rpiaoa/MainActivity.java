5f5f39e633d22bbe38876a1bfd1d65bcc92f05f6
Daniel Hilton <hilton.daniel0@gmail.com>
2014-01-09 14:56:34 +0100

    Added gyro support to write to log files, TODO protocol for transmission

--- RaspberryPi/AOA_App/src/com/danhil/rpiaoa/MainActivity.java
+++ RaspberryPi/AOA_App/src/com/danhil/rpiaoa/MainActivity.java
@@ -1,9 +1,14 @@
 package com.danhil.rpiaoa;
 
+import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 
 import com.android.future.usb.UsbAccessory;
 import com.android.future.usb.UsbManager;
@@ -14,7 +19,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.os.ParcelFileDescriptor;
@@ -75,11 +85,35 @@ public class MainActivity extends Activity implements Runnable {
 			}
 		}
 	};
+	
+	//GyroVariables
+	private SensorManager mSensorManager;
+	private Sensor mGyroSensor, mAccSensor, mMagSensor;
+	public static final String TAG1 = "SensingService";
+	private static String fileGyro,fileAcc,fileMag;
+	private static FileOutputStream foutGyro, foutAcc, foutMag;
+	public static File FILEPATH;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		// GYRO INIT
+		Log.e(TAG, "onCreateGyro");
+		FILEPATH = new File(Environment.getExternalStorageDirectory().getPath() + "/SensorSensing/");
+		FILEPATH.mkdirs();
+		Log.e(TAG, "Path for gyrologs is :" + FILEPATH);
 
+		prepareFiles();
+
+		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+		mGyroSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+		mAccSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		mMagSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+		
+		
+		// AOA COMMUNICATION
 		usbManagerMobile = UsbManager.getInstance(this);
 		permissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(
 				ACTION_USB_PERMISSION), 0);
@@ -108,6 +142,8 @@ public class MainActivity extends Activity implements Runnable {
 				sendCommand(command, value);
 			}
 		});
+		
+		
 
 		seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 			@Override
@@ -134,11 +170,154 @@ public class MainActivity extends Activity implements Runnable {
 
 		enableControls(false);
 	}
+	
+	private SensorEventListener mGyroListener = new SensorEventListener() {
+
+		private static final float MIN_TIME_STEP = (1f / 40f);
+		private long mLastTime = System.currentTimeMillis();
+		private float mRotationX, mRotationY, mRotationZ;
+
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		}
+
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			float[] values = event.values;
+			float x = values[0];
+			float y = values[1];
+			float z = values[2];
+			String logString;
+
+			float angularVelocity = z * 0.96f; // Minor adjustment to avoid drift on Nexus
+
+			// Calculate time diff
+			long now = System.currentTimeMillis();
+			float timeDiff = (now - mLastTime) / 1000f;
+			mLastTime = now;
+			if (timeDiff > 1) {
+				// Make sure we don't go bananas after pause/resume
+				timeDiff = MIN_TIME_STEP;
+			}
+
+			mRotationX += x * timeDiff;
+			if (mRotationX > 0.5f)
+				mRotationX = 0.5f;
+			else if (mRotationX < -0.5f)
+				mRotationX = -0.5f;
+
+			mRotationY += y * timeDiff;
+			if (mRotationY > 0.5f)
+				mRotationY = 0.5f;
+			else if (mRotationY < -0.5f)
+				mRotationY = -0.5f;
+
+			mRotationZ += angularVelocity * timeDiff;
+
+			try {
+				logString = ""+ timeDiff + " " + mRotationX + " " + mRotationY + " " + mRotationZ + "\n";
+				foutGyro.write(logString.getBytes());
+				foutGyro.flush();
+			} catch (IOException e) {
+				Log.e("IOError",e.toString());
+			}
+		}
+	};
+
+	private SensorEventListener mAccListener = new SensorEventListener() {
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		}
+
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			float[] values = event.values;
+			float x = values[0];
+			float y = values[1];
+			// float z = values[2];
+			String logString;
+
+			// Ignoring orientation since the activity is using screenOrientation "nosensor"
+			try {
+				logString = " " + -x + " " + y + " " + "\n";
+				foutAcc.write(logString.getBytes());
+				foutAcc.flush();
+			} catch (IOException e) {
+				Log.e("IOError",e.toString());
+			}
+		}
+	};
+
+	private SensorEventListener mMagListener = new SensorEventListener() {
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		}
+
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			float[] values = event.values;
+			float x = values[0];
+			float y = values[1];
+			// float z = values[2];
+			String logString;
+			try {
+				logString = " " + x + " " + -y + " " + "\n";
+				foutMag.write(logString.getBytes());
+				foutMag.flush();
+			} catch (IOException e) {
+				Log.e("IOError",e.toString());
+			}
+		}
+	};
+
+	public void prepareFiles(){
+		Log.d("prepareFiles", "initialized");
+
+		//create a new file for gyro
+		try {
+			Log.e("Log","Write");
+			fileAcc = FILEPATH+"/"+getCurrentTimeStamp()+"-accl.txt";
+			foutAcc = new FileOutputStream(fileAcc,true);
+		} catch (IOException e) {
+			Log.e("IOError",e.toString());
+		}
+		//create a new file for accelorometer
+		try {
+			Log.e("Log","Write");
+			fileGyro = FILEPATH+"/"+getCurrentTimeStamp()+"-gyro.txt";
+			foutGyro = new FileOutputStream(fileGyro,true);
+		} catch (IOException e) {
+			Log.e("IOError",e.toString());
+		}
+		//create a new file for mag
+		try {
+			fileMag = FILEPATH+"/"+getCurrentTimeStamp()+"-mag.txt";
+			foutMag = new FileOutputStream(fileMag,true);
+		} catch (IOException e) {
+			Log.e("IOError",e.toString());
+		}
+
+		Log.d("prepareFiles", "excecuted");
+	}
+
+	public static String getCurrentTimeStamp() {
+		Locale loc = Locale.getDefault();
+		SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss", loc);
+		Date now = new Date();
+		String strDate = sdfDate.format(now);
+		return strDate;
+	}
 
 	@Override
 	public void onResume() {
 		super.onResume();
-
+		//GYRO
+		mSensorManager.registerListener(mGyroListener, mGyroSensor, SensorManager.SENSOR_DELAY_UI);
+		mSensorManager.registerListener(mAccListener, mAccSensor, SensorManager.SENSOR_DELAY_UI);
+		mSensorManager.registerListener(mMagListener, mMagSensor, SensorManager.SENSOR_DELAY_UI);
+		prepareFiles();
+		
+		// AOA USB communication
 		if (inputStream != null && outputStream != null) {
 			return;
 		}
@@ -165,11 +344,33 @@ public class MainActivity extends Activity implements Runnable {
 	@Override
 	public void onPause() {
 		super.onPause();
+		// GYRO
+		mSensorManager.unregisterListener(mGyroListener, mGyroSensor);
+		mSensorManager.unregisterListener(mAccListener, mGyroSensor);
+		mSensorManager.unregisterListener(mMagListener, mMagSensor);
+		finishFiles();
+		//USB
 		closeAccessory();
+	}
+	
+	public void finishFiles(){
+		try{
+			//	foutAcc.flush();
+			foutAcc.close();
+			//	foutGyro.flush();
+			foutGyro.close();
+			//	foutMag.flush();
+			foutMag.close();
+		}catch(IOException e){
+			Log.e("IOException","Error on closing files");
+		}
 	}
 
 	@Override
 	public void onDestroy() {
+		// GYRO
+		finishFiles();
+		//USB
 		unregisterReceiver(usbRecieve);
 		super.onDestroy();
 	}
