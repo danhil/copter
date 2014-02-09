0d2d96e2cb9bae6f0a5b3fbb5a64fd961c6cd0ab
Daniel Hilton <hilton.daniel0@gmail.com>
2014-02-09 18:05:07 +0100

    Refactored

--- RaspberryPi/AOA_App/src/com/danhil/rpiaoa/MainActivity.java
+++ RaspberryPi/AOA_App/src/com/danhil/rpiaoa/MainActivity.java
@@ -33,20 +33,14 @@ import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements Runnable, SensorsObserver {
+public class MainActivity extends Activity implements SensorsObserver {
 
 	private static final String TAG = MainActivity.class.getSimpleName();
-	private static final String ACTION_USB_PERMISSION = "com.danhil.rpiaoa.action.USB_PERMISSION";
 	protected static File FILEPATH;
 	private static String fileSensors;
 	private static FileOutputStream foutSensors;
-	private PendingIntent permissionIntent;
-	private boolean requestPermissonPending;
-	private UsbManager usbManagerMobile;
-	private UsbAccessory accessoryForMobile;
-	private ParcelFileDescriptor fileDescriptor;
-	private FileInputStream inputStream;
-	private FileOutputStream outputStream;
+	private USBcontroller usbController;
+
 	private SeekBar pBar;
 	private SeekBar iBar;
 	private SeekBar dBar;
@@ -72,38 +66,7 @@ public class MainActivity extends Activity implements Runnable, SensorsObserver 
 	//TODO Why is this vector 5 long?
 	private float[] inputSensorValues = {0, 0, 0};
 
-	private final BroadcastReceiver usbRecieve = new BroadcastReceiver(){
-
-		@Override
-		public void onReceive(Context context, Intent intent) {
-
-			String action = intent.getAction();
-			if (ACTION_USB_PERMISSION.equals(action))
-			{
-				synchronized (this)
-				{
-					UsbAccessory usbAccessory = UsbManager.getAccessory(intent);
-
-					if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false))
-					{
-						openAccessory(usbAccessory);
-					} else
-					{
-						Log.d(TAG, "Accesory permission denied " + usbAccessory);
-					}
-
-					requestPermissonPending = false;
-				}
-			} else if (UsbManager.ACTION_USB_ACCESSORY_DETACHED.equals(action))
-			{
-				UsbAccessory usbAccessory = UsbManager.getAccessory(intent);
-				if (usbAccessory != null && usbAccessory.equals(accessoryForMobile))
-				{
-					closeAccessory();
-				}
-			}
-		}
-	};
+	
 	
 
 	@Override
@@ -117,23 +80,7 @@ public class MainActivity extends Activity implements Runnable, SensorsObserver 
 		prepareFileStreams();
 		sensors = new Sensors(this, foutSensors);
 		sensors.registerSensorsObserver(this);
-
-		/* Init usbmanager and AOA COMMUNICATION */ 	 	
-		usbManagerMobile = UsbManager.getInstance(this);
-		permissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(
-				ACTION_USB_PERMISSION), 0);
-		// Filter on intent charastaristics.
-		IntentFilter filter = new IntentFilter();
-		filter.addAction(ACTION_USB_PERMISSION);
-		filter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
-		registerReceiver(usbRecieve, filter);
-
-		if (getLastNonConfigurationInstance() != null)
-		{ //Use fragments instead.
-			accessoryForMobile = (UsbAccessory) getLastNonConfigurationInstance();
-			openAccessory(accessoryForMobile);
-		}
-		
+		usbController = new USBcontroller(this, handler);
 		PID = new PIDController(90, 1, 1, 1);
 		setContentView(R.layout.activity_main);
 		statusView = (TextView) findViewById(R.id.statusIndication);
@@ -238,39 +185,11 @@ public class MainActivity extends Activity implements Runnable, SensorsObserver 
 	{
 		super.onResume();
 		sensors.onStart();
-		restartAccessory();
+		usbController.restartAccessory();
 
 	}
 
-	private void restartAccessory()
-	{
-		if (inputStream != null && outputStream != null)
-		{
-			return;
-		}
-		UsbAccessory[] accessories = usbManagerMobile.getAccessoryList();
-		UsbAccessory accessory = (accessories == null ? null : accessories[0]);
-		if (accessory != null)
-		{
-			if (usbManagerMobile.hasPermission(accessory))
-			{
-				openAccessory(accessory);
-			} else {
-				synchronized (usbRecieve)
-				{
-					if (!requestPermissonPending)
-					{
-						usbManagerMobile.requestPermission(accessory,
-								permissionIntent);
-						requestPermissonPending = true;
-					}
-				}
-			}
-		} else {
-			Log.d(TAG, "mAccessory is null");
-		}
-
-	}
+	
 
 	@Override
 	public void onPause()
@@ -278,7 +197,7 @@ public class MainActivity extends Activity implements Runnable, SensorsObserver 
 		super.onPause();
 		sensors.onPause();
 		//USB
-		closeAccessory();
+		usbController.closeAccessory();
 	}
 
 
@@ -287,7 +206,7 @@ public class MainActivity extends Activity implements Runnable, SensorsObserver 
 	public void onDestroy()
 	{
 		// Unregister the usB
-		unregisterReceiver(usbRecieve);
+		usbController.unregisterController();
 		finishFiles();
 		super.onDestroy();
 	}
@@ -302,48 +221,7 @@ public class MainActivity extends Activity implements Runnable, SensorsObserver 
 		}
 	}
 
-	private void openAccessory(UsbAccessory accessory)
-	{
-		fileDescriptor = usbManagerMobile.openAccessory(accessory);
-
-		if (fileDescriptor != null)
-		{
-			accessoryForMobile = accessory;
-			FileDescriptor fd = fileDescriptor.getFileDescriptor();
-
-			inputStream = new FileInputStream(fd);
-			outputStream = new FileOutputStream(fd);
-
-			// communication thread start
-			Thread thread = new Thread(null, this, "CommThread");
-			thread.start();
-			Log.d(TAG, "Opened accessory");
-
-			setConnectedStatus(true);
-		} else
-		{
-			Log.d(TAG, "accessory didnt open");
-		}
-	}
-
-	private void closeAccessory()
-	{
-		setConnectedStatus(false);
-
-		try
-		{
-			if (fileDescriptor != null)
-			{
-				fileDescriptor.close();
-			}
-		} catch (IOException e)
-		{
-		} finally 
-		{
-			fileDescriptor = null;
-			accessoryForMobile = null;
-		}
-	}
+	
 
 	private void setConnectedStatus(boolean enable)
 	{
@@ -358,55 +236,7 @@ public class MainActivity extends Activity implements Runnable, SensorsObserver 
 
 	private static final int MESSAGE_LED = 1;
 
-	@Override
-	public void run()
-	{
-		int ret = 0;
-		byte[] reciveBuffer = new byte[16384];
-		int i;
-
-		// Accessory -> Android, ReadThread
-		while (ret >= 0)
-		{
-			try
-			{
-				ret = inputStream.read(reciveBuffer);
-			} catch (IOException e)
-			{
-				e.printStackTrace();
-				break;
-			}
-
-			if (ret > 0)
-			{
-				Log.d(TAG, ret + " bytes message received.");
-			}
-			i = 0;
-			while (i < ret)
-			{
-				int len = ret - i;
-
-				switch (reciveBuffer[i])
-				{
-				case 0x1:
-					if (len >= 2)
-					{
-						Message m = Message.obtain(handler, MESSAGE_LED);
-						m.arg1 = (int) reciveBuffer[i + 1];
-						handler.sendMessage(m);
-						i += 2;
-					}
-					break;
-
-				default:
-					Log.d(TAG, "unknown msg: " + reciveBuffer[i]);
-					i = len;
-					break;
-				}
-			}
-
-		}
-	}
+	
 
 	// Change the view in the UI thread, handler is static so no leaks occur
 	private static Handler handler = new Handler()
@@ -429,47 +259,7 @@ public class MainActivity extends Activity implements Runnable, SensorsObserver 
 		}
 	};
 
-	// Android -> Accessory
-	public void sendCommand(byte command, byte value)
-	{
-		byte[] sendBuffer = new byte[2];
-		sendBuffer[0] = command;
-		sendBuffer[1] = value;
-		if (outputStream != null)
-		{
-			try 
-			{
-				outputStream.write(sendBuffer);
-			} catch (IOException e) {
-				Log.e(TAG, "Failed Write Android->Acc", e);
-			}
-		}
-	}
-
-	public void sendCommand(byte[] command, byte[] value)
-	{
-		byte[] sendBuffer = concat(command,value);
-		Log.d("sendCommand", "Sent the command: \n" + new String(sendBuffer));
-		if (outputStream != null)
-		{
-			try 
-			{
-				outputStream.write(sendBuffer, 0, 8);
-			} catch (IOException e) {
-				Log.e(TAG, "Failed Write Android->Acc", e);
-			}
-		}
-	}
-
-	byte[] concat(byte[] A, byte[] B) {
-		int aLen = A.length;
-		int bLen = B.length;
-		byte[] res= new byte[aLen+bLen];
-		System.arraycopy(A, 0, res, 0, aLen);
-		System.arraycopy(B, 0, res, aLen, bLen);
-		return res;
-	}
-
+	
 	@Override
 	public void onSensorsChanged(float[] inputSensorValues, long timeStamp)
 	{
@@ -482,7 +272,7 @@ public class MainActivity extends Activity implements Runnable, SensorsObserver 
 		double controlOutput = PID.getOutput();
 		String controlOutputString = Double.toString(controlOutput);
 		byte[] transmitX = controlOutputString.getBytes();
-		sendCommand(command, transmitX);
+		usbController.sendCommand(command, transmitX);
 		tiltView.setText(Double.toString(z*(180/Math.PI)));
 		controlView.setText(controlOutputString);
 		
---
+++ RaspberryPi/AOA_App/src/com/danhil/rpiaoa/USBcontroller.java
@@ -0,0 +1,263 @@
+package com.danhil.rpiaoa;
+
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Handler;
+import android.os.Message;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
+
+import com.android.future.usb.UsbAccessory;
+import com.android.future.usb.UsbManager;
+
+public class USBcontroller implements Runnable {
+	
+	private PendingIntent permissionIntent;
+	private boolean requestPermissonPending;
+	private UsbManager usbManagerMobile;
+	private UsbAccessory accessoryForMobile;
+	private ParcelFileDescriptor fileDescriptor;
+	private FileInputStream inputStream;
+	private FileOutputStream outputStream;
+	private Context context;
+	private Handler handler;
+	private static final String ACTION_USB_PERMISSION = "com.danhil.rpiaoa.action.USB_PERMISSION";
+	private static final String TAG = USBcontroller.class.getSimpleName();
+
+	public USBcontroller(Context parentContext, Handler handler)
+	{
+		this.handler = handler;
+		/* Init usbmanager and AOA COMMUNICATION */ 	 	
+		usbManagerMobile = UsbManager.getInstance(parentContext);
+		permissionIntent = PendingIntent.getBroadcast(parentContext, 0, new Intent(
+				ACTION_USB_PERMISSION), 0);
+		/*if (getLastNonConfigurationInstance() != null)
+		{ //Use fragments instead.
+			accessoryForMobile = (UsbAccessory) getLastNonConfigurationInstance();
+			openAccessory(accessoryForMobile);
+		}*/
+		// Filter on intent charastaristics.
+		IntentFilter filter = new IntentFilter();
+		filter.addAction(ACTION_USB_PERMISSION);
+		filter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
+		parentContext.registerReceiver(usbRecieve, filter);
+		
+	}
+	
+	private final BroadcastReceiver usbRecieve = new BroadcastReceiver(){
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+
+			String action = intent.getAction();
+			if (ACTION_USB_PERMISSION.equals(action))
+			{
+				synchronized (this)
+				{
+					UsbAccessory usbAccessory = UsbManager.getAccessory(intent);
+
+					if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false))
+					{
+						openAccessory(usbAccessory);
+					} else
+					{
+						Log.d(TAG, "Accesory permission denied " + usbAccessory);
+					}
+
+					requestPermissonPending = false;
+				}
+			} else if (UsbManager.ACTION_USB_ACCESSORY_DETACHED.equals(action))
+			{
+				UsbAccessory usbAccessory = UsbManager.getAccessory(intent);
+				if (usbAccessory != null && usbAccessory.equals(accessoryForMobile))
+				{
+					closeAccessory();
+				}
+			}
+		}
+	};
+	
+	public void restartAccessory()
+	{
+		if (inputStream != null && outputStream != null)
+		{
+			return;
+		}
+		UsbAccessory[] accessories = usbManagerMobile.getAccessoryList();
+		UsbAccessory accessory = (accessories == null ? null : accessories[0]);
+		if (accessory != null)
+		{
+			if (usbManagerMobile.hasPermission(accessory))
+			{
+				openAccessory(accessory);
+			} else {
+				synchronized (usbRecieve)
+				{
+					if (!requestPermissonPending)
+					{
+						usbManagerMobile.requestPermission(accessory,
+								permissionIntent);
+						requestPermissonPending = true;
+					}
+				}
+			}
+		} else {
+			Log.d(TAG, "mAccessory is null");
+		}
+
+	}
+	
+	private void openAccessory(UsbAccessory accessory)
+	{
+		fileDescriptor = usbManagerMobile.openAccessory(accessory);
+
+		if (fileDescriptor != null)
+		{
+			accessoryForMobile = accessory;
+			FileDescriptor fd = fileDescriptor.getFileDescriptor();
+
+			inputStream = new FileInputStream(fd);
+			outputStream = new FileOutputStream(fd);
+
+			// communication thread start
+			Thread thread = new Thread(null, this, "CommThread");
+			thread.start();
+			Log.d(TAG, "Opened accessory");
+
+			sendToHandler(1);
+		} else
+		{
+			Log.d(TAG, "accessory didnt open");
+		}
+	}
+
+	public void closeAccessory()
+	{
+		sendToHandler(0);
+
+		try
+		{
+			if (fileDescriptor != null)
+			{
+				fileDescriptor.close();
+			}
+		} catch (IOException e)
+		{
+		} finally 
+		{
+			fileDescriptor = null;
+			accessoryForMobile = null;
+		}
+	}
+	
+	public void unregisterController()
+	{
+		context.unregisterReceiver(usbRecieve);
+	}
+	
+	private void sendToHandler(int i)
+	{
+		Message m = Message.obtain(handler, i);
+		m.arg1 = (int) i;
+		handler.sendMessage(m);
+	}
+	
+	@Override
+	public void run()
+	{
+		int ret = 0;
+		byte[] reciveBuffer = new byte[16384];
+		int i;
+
+		// Accessory -> Android, ReadThread
+		while (ret >= 0)
+		{
+			try
+			{
+				ret = inputStream.read(reciveBuffer);
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+				break;
+			}
+
+			if (ret > 0)
+			{
+				Log.d(TAG, ret + " bytes message received.");
+			}
+			i = 0;
+			while (i < ret)
+			{
+				int len = ret - i;
+
+				switch (reciveBuffer[i])
+				{
+				case 0x1:
+					if (len >= 2)
+					{
+						sendToHandler((int) reciveBuffer[i + 1]);
+						i += 2;
+					}
+					break;
+
+				default:
+					Log.d(TAG, "unknown msg: " + reciveBuffer[i]);
+					i = len;
+					break;
+				}
+			}
+
+		}
+	}
+	
+	// Android -> Accessory
+		public void sendCommand(byte command, byte value)
+		{
+			byte[] sendBuffer = new byte[2];
+			sendBuffer[0] = command;
+			sendBuffer[1] = value;
+			if (outputStream != null)
+			{
+				try 
+				{
+					outputStream.write(sendBuffer);
+				} catch (IOException e) {
+					Log.e(TAG, "Failed Write Android->Acc", e);
+				}
+			}
+		}
+
+		public void sendCommand(byte[] command, byte[] value)
+		{
+			byte[] sendBuffer = concat(command,value);
+			Log.d("sendCommand", "Sent the command: \n" + new String(sendBuffer));
+			if (outputStream != null)
+			{
+				try 
+				{
+					outputStream.write(sendBuffer, 0, sendBuffer.length);
+				} catch (IOException e) {
+					Log.e(TAG, "Failed Write Android->Acc", e);
+				}
+			}
+		}
+
+		byte[] concat(byte[] A, byte[] B) {
+			int aLen = A.length;
+			int bLen = B.length;
+			byte[] res= new byte[aLen+bLen];
+			System.arraycopy(A, 0, res, 0, aLen);
+			System.arraycopy(B, 0, res, aLen, bLen);
+			return res;
+		}
+
+
+}
